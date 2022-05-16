723a99689df52393f65dc183771561f32415ef8f
Eugene Multan <eugene.multan@gmail.com>
2022-05-16 23:32:12 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,23 @@
+const refs = {
+  form: document.querySelector('form'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const myPromise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+      resolve('Success! Value passed to resolve function');
+    } else {
+      // Reject
+      reject('Error! Error passed to reject function');
+    }
+  });
 }
+
+const handleSubmit = event => {
+  event.preventDefault();
+  createPromise();
+};
+
+refs.form.addEventListener('submit', handleSubmit);
